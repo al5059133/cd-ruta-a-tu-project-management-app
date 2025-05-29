91d688043577c3bbb0cf2fb83decb53a90a4d70f
al5059133 <al05059133@tecmilenio.mx>
2025-05-28 23:16:36 -0600

    Add files via upload

---
+++ Fase 1 Full STACK 2025.js
@@ -0,0 +1,12 @@
+const config = {
+  "homepage": "https://ACC F STACK 2025.github.io/ACC F STACK 2025",
+
+  "scripts": {
+    "predeploy": "npm run build",
+    "deploy": "gh-pages -d build",
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+  }
+};
